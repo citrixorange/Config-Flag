3885e270114f1ed5c252820d1d1d084838378a15
citrixorange <caio.cb.sp@gmail.com>
2024-01-18 10:48:13 -0300

    Using Error handler on Config Class

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,15 +2,28 @@
 
 int main() {
 
-    string config_name("CONFIG1");
+    string config_name_one("CONFIG1");
+    string config_name_two("CONFIG2");
     string config_value("CONFIG_VALUE_1");
+    
     Config<string> config;
-    config.set_parameter(config_name, config_value);
-    Result<string> fetched_config = config.get_parameter(config_name);
+    config.set_parameter(config_name_one, config_value);
+    
+    Result<string> fetched_config = config.get_parameter(config_name_one);
+
     if(holds_alternative<string>(fetched_config)) {
         cout << get<string>(fetched_config) << endl;
     } else {
-        cout << "Error" << endl;
+        cout << get<ConfigError>(fetched_config) << endl;
     }
+
+    fetched_config = config.get_parameter(config_name_two);
+
+    if(holds_alternative<string>(fetched_config)) {
+        cout << get<string>(fetched_config) << endl;
+    } else {
+        cout << get<ConfigError>(fetched_config) << endl;
+    }
+
     return 0;
 }
--- static_lib/src/config_flag.cpp
+++ static_lib/src/config_flag.cpp
@@ -2,6 +2,24 @@
 
 template class Config<string>;
 
+vector<tuple<ConfigError, string>> error_messages = {
+    {CONFIG_NOT_FOUND, "Configuration not found."},
+    {CONFIG_ALREADY_EXIST, "Configuration already exists."},
+    {UNEXPECTED_ERROR, UNEXPECTED_ERROR_MESSAGE}
+};
+
+ConfigError error_handler_cb(tuple<ConfigError, string> handler) {
+    cout << "Error: " << get<string>(handler) << endl;
+    return get<ConfigError>(handler);
+}
+
+template<class T>
+Config<T>::Config() {
+    this->error_handler.registerErrorMessages(error_messages);
+    this->error_handler.registerErrorCallback(ConfigError::CONFIG_ALREADY_EXIST, error_handler_cb);
+    this->error_handler.registerErrorCallback(ConfigError::CONFIG_NOT_FOUND, error_handler_cb);
+}
+
 template<class T>
 void Config<T>::set_parameter(string config, T value) {
     this->configs[config] = value;
@@ -12,6 +30,14 @@ Result<T> Config<T>::get_parameter(string config) {
     if(this->configs.find(config) != this->configs.end()) {
         return this->configs[config];
     } else {
-        return ConfigError::CONFIG_NOT_FOUND;
+
+        
+        return (this->error_handler.handleError(
+            ConfigError::CONFIG_NOT_FOUND, 
+            make_tuple(
+                ConfigError::CONFIG_NOT_FOUND, 
+                this->error_handler.getErrorMessage(ConfigError::CONFIG_NOT_FOUND).value_or(UNEXPECTED_ERROR_MESSAGE)
+            )
+        )).value_or(ConfigError::UNEXPECTED_ERROR);
     }
 }
